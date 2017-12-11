e9d36b05e8375085281abdf09ebf28907d41d458
mallinookala03 <33166044+mallinookala03@users.noreply.github.com>
2017-12-10 16:29:15 -0800

    Adding R file and generated images.

--- plot2.R
+++ plot2.R
@@ -2,9 +2,8 @@ dataFilePath <- "./data/household_power_consumption.txt"
 fileData <- read.table(dataFilePath, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- fileData[fileData$Date %in% c("1/2/2007","2/2/2007") ,]
 
-
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
-png("plot1.png", width=480, height=480)
-hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
-
